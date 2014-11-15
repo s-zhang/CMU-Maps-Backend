997e9b10c45da88f3abed1b63e54559ceb72e7cd
Steven Zhang <szh10000@gmail.com>
2014-11-15 11:33:50 -0500

    Removed spatial query support for Room and Floor

--- app/models.py
+++ app/models.py
@@ -16,8 +16,6 @@ class Floor(models.Model):
     building = models.ForeignKey(Building)
     plan = models.FileField(upload_to="plans")
 
-    objects = models.GeoManager()
-
     def __unicode__(self):
         return self.building.name + "_" + self.name
 
@@ -26,8 +24,6 @@ class Room(models.Model):
     floor = models.ForeignKey(Floor)
     building = models.ForeignKey(Building)
     anchor = models.PointField()
-    
-    objects = models.GeoManager()
 
     def __unicode__(self):
         return self.building.name + "_" + self.name
