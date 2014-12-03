49b77517061b5610fba4a3081c429f7e009dea8e
Steven Zhang <szh10000@gmail.com>
2014-12-02 20:40:51 -0500

    Added urls.py

--- cmumaps/urls.py
+++ cmumaps/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from app.views import display, query, route
+
+float_pattern = '[1-9][0-9]*\.?[0-9]*'
+int_pattern = '[1-9][0-9]*'
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^display/' + ('(' + float_pattern + ')/') * 4 + '(\S*)/$', display),
+    url(r'^query/' + '(' + int_pattern + ')/$', query),
+    url(r'^route/' + ('(' + int_pattern + ')/') * 3 + '$', route)
 )
